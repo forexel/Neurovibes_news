f66122004b56b296ade567523e8ba24d848dc120
forexel <danick@inbox.ru>
2026-03-04 23:48:41 +0300

    Align root layout shell with News Publish

--- admin-web/src/app/components/Root.tsx
+++ admin-web/src/app/components/Root.tsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router";
-import { ThemeProvider } from "next-themes";
 import { Toaster } from "./ui/sonner";
 
 export default function Root() {
   return (
-    <ThemeProvider attribute="class" forcedTheme="dark">
-      <div className="dark min-h-screen bg-background text-foreground">
-        <Outlet />
-        <Toaster />
-      </div>
-    </ThemeProvider>
+    <div className="dark min-h-screen bg-background text-foreground">
+      <Outlet />
+      <Toaster />
+    </div>
   );
 }
